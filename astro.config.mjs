4eb773dc227e9979aeee6448e8e6a2bd6a8949b6
simon-castillo <simon.castillo@digital.homeoffice.gov.uk>
2024-12-31 11:29:30 +0000

    Added comma to mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://simcast.github.io',
-    base: '/simon-castillo'
+    base: '/simon-castillo',
 });
