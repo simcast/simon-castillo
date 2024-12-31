b6280a4fe00e67eb483180c1f19e878aae6eccb9
simon-castillo <simon.castillo@digital.homeoffice.gov.uk>
2024-12-31 11:02:13 +0000

    added base to mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://simcast.github.io/simon-castillo'
+    site: 'https://simcast.github.io',
+    base: 'simon-castillo'
 });
